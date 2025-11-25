6e9633ab00db37765fe343e0572d53325f636cb9
Ahmed Abdullah <bn36203@gmail.com>
2025-11-25 20:06:09 +0200

    Update Numerical_UI.h

--- Numerical_UI.h
+++ Numerical_UI.h
@@ -4,6 +4,12 @@
 
 #include "Numerical_Board.h"
 
+class NumericalUI {#pragma once
+#ifndef NUMERICAL_UI_H
+#define NUMERICAL_UI_H
+
+#include "Numerical_Board.h"
+
 class NumericalUI {
 private:
     NumericalBoard& board;
@@ -15,4 +21,40 @@ public:
     void getMove(int& row, int& col); // Get row and column
 };
 
+class SUSUI {
+private:
+    SUSBoard& board;
+    char playerLetter[3];
+    int score[3];
+
+    void getMove(int& row, int& col);
+    char choosePlayer1Letter();
+
+public:
+    SUSUI(SUSBoard& b);
+    void playGame();
+};
+
+class ObstaclesUI {
+private:
+    ObstaclesBoard& board;
+
+public:
+    ObstaclesUI(ObstaclesBoard& b);
+    void playGame();
+    void getMove(int& row, int& col, int player);
+};
+
 #endif
+private:
+    NumericalBoard& board;
+
+public:
+    NumericalUI(NumericalBoard& b);
+    void playGame();           // Main loop of the game
+    int getPlayerInput(int player);  // Get number from player
+    void getMove(int& row, int& col); // Get row and column
+};
+
+#endif
+
